d382906d06d5d346c43577ca8862d4b00e015d57
Saif Medhat <seifmedhatfarouk@gmail.com>
2026-03-05 21:03:08 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Yet_Another_Examination_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
